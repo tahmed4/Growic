c5dde595f4e47de4b58b544f88db6c66369eba5d
Thowhid Ahmed <55896104+tahmed4@users.noreply.github.com>
2022-08-16 21:03:41 +0100

    Completed(Task 5) - Payable

---
+++ contracts/payable.sol
@@ -0,0 +1,25 @@
+pragma solidity >=0.8.0 <0.9.0;
+//SPDX-License-Identifier: MIT
+
+import "hardhat/console.sol";
+import "@openzeppelin/contracts/access/Ownable.sol"; 
+// https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol
+
+contract YourContract {
+
+  event RegisterBalance(address user, uint256 amount);
+
+  mapping(address => uint) public balances;
+
+  constructor() {
+  }
+
+  function deposit() public payable {
+      balances[msg.sender] += msg.value;
+      emit RegisterBalance(msg.sender, msg.value);
+  }
+
+  function checkBalance(address _user) public view returns(uint256) {
+    return(balances[_user]);
+  }
+}
